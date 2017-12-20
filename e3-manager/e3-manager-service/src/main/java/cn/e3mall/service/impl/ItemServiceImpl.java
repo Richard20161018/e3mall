c0dbf48dfd0a570ea04d7ad737623c0db3516950
RichardWang <2336400051@qq.com>
2017-12-20 23:02:36 +0800

    20171220提交

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.pojo.TbItemExample;
@@ -38,6 +42,22 @@ public class ItemServiceImpl implements ItemService {
 			return list.get(0);
 		}
 		return null;
+	}
+
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//创建一个返回对象
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> items=itemMapper.selectByExample(example);
+		result.setRows(items);
+		PageInfo<TbItem> pageInfo = new PageInfo<>(items);
+		result.setTotal(pageInfo.getTotal());
+		
+		return result;
 	}
 
 }
